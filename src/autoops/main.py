ab0a0e55b741cea875a60d3c27ed43fc9b7c083d
TriptiP-Code <triptip033@gmail.com>
2026-08-10 00:18:15 +0530

    feat(cli): read command-line arguments

---
+++ src/autoops/main.py
@@ -0,0 +1,12 @@
+import sys
+
+
+def start_application():
+    print("===================================")
+    print("🚀 Starting AutoOps...")
+    print("===================================")
+
+
+start_application()
+
+print(sys.argv)
